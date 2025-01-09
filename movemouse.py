8a371452b3773f67867c97d947cbcf4fe13d1b6b
Yuki-Yui <yuukiyuinyan@gmail.com>
2025-01-09 19:59:34 +0900

    Pythonでwin32api叩けてアツい

--- movemouse.py
+++ movemouse.py
@@ -5,6 +5,7 @@ import mediapipe as mp
 import time
 import webbrowser  # URLを開くために必要
 import math
+import win32api
 
 import pyautogui
 pyautogui.FAILSAFE = False
@@ -36,6 +37,9 @@ url2 = "https://www.uec.ac.jp"  # 開きたいURL2を指定
 def calculate_distance(x1, y1, x2, y2):
     return math.hypot(x2 - x1, y2 - y1)
 
+def mouse_move(x, y):
+    win32api.SetCursorPos((x, y))
+
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
@@ -45,7 +49,9 @@ while True:
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             landmarks = {}
-            for id, lm in enumerate(handLms.landmark):
+            # for id, lm in enumerate(handLms.landmark):
+            for id in [0, 2, 4, 8, 12, 16, 17, 20]: # 手首、親指付け根、親指先端、人差し指先端、中指先端、薬指先端、小指付け根、小指先端
+                lm = handLms.landmark[id]
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 landmarks[id] = (cx, cy)
@@ -54,8 +60,6 @@ while True:
                 if id in [4, 12, 16]:
                     cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
-            if len(landmarks) < 20:
-                continue
 
             # 手の大きさ（親指付け根と小指付け根の距離）
             hand_size = calculate_distance(landmarks[2][0], landmarks[2][1], landmarks[17][0], landmarks[17][1])
@@ -67,11 +71,12 @@ while True:
             distance_0to2 = calculate_distance(thumb_tip[0], thumb_tip[1], y2_tip[0], y2_tip[1])
             distance_0to3 = calculate_distance(thumb_tip[0], thumb_tip[1], y3_tip[0], y3_tip[1])
 
-            distance_threshold = 0.3
+            distance_threshold = 0.4
 
             if distance_0to2 / hand_size < distance_threshold and distance_0to3 / hand_size < distance_threshold:
 
-                current_mouse_x, current_mouse_y = pyautogui.position()
+                # current_mouse_x, current_mouse_y = pyautogui.position()
+                current_mouse_x, current_mouse_y = win32api.GetCursorPos()
                 wrist_x, wrist_y = landmarks[0] # 手首の位置
                 positions.append([wrist_x, wrist_y])
                 if len(positions) > max_positions:
@@ -93,8 +98,11 @@ while True:
                     
                     # print(avg_motion_distance_x, motion_distance_x)
 
-                    mouse_move_x = current_mouse_x + avg_motion_distance_x * kando
-                    mouse_move_y = current_mouse_y + avg_motion_distance_y * kando
+                    mouse_x = current_mouse_x + avg_motion_distance_x * kando
+                    mouse_y = current_mouse_y + avg_motion_distance_y * kando
+
+                    mouse_x = int(mouse_x)
+                    mouse_y = int(mouse_y)
 
                     # print(motion_distance_x, motion_distance_y)
 
@@ -103,7 +111,8 @@ while True:
 
                     # print(mouse_move_x, mouse_move_y)
 
-                    pyautogui.moveTo(mouse_move_x, mouse_move_y)
+                    # pyautogui.moveTo(mouse_move_x, mouse_move_y)
+                    win32api.SetCursorPos((mouse_x, mouse_y))
 
                 moving = True
             else:
@@ -117,7 +126,7 @@ while True:
     fps = 1 / (cTime - pTime)
     pTime = cTime
     cv2.putText(img, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-    cv2.putText(img, f'Bool: {not(moving)}', (10, 140), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.putText(img, f'Moving: {moving}', (10, 140), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
     # 画像を表示
     cv2.imshow("Image", img)
